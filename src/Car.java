14befcfcd97dd9a936e2675bd07274a243cefbe0
Istiak_Ahamed <istiakop800@gmail.com>
2026-05-23 13:31:28 +0600

    CarOBject

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    String make = "Bugatti Chiron";
+    String model = "Chiron Super Sport";
+    int year = 2024;
+    String color = "Blue";
+    String fuelType = "Petrol";
+    String topSpeed = "490 km/h";
+    double price = 491780656.00;//4 million dollar
+    boolean isRunning = false;
+
+}
---
+++ src/CarTest.java
@@ -0,0 +1,14 @@
+public class CarTest {
+    public static void main(String[] args) {
+        Car c1 = new Car ();
+
+        System.out.println(c1.make);
+        System.out.println(c1.model);
+        System.out.println(c1.year);
+        System.out.println(c1.price);
+        System.out.println(c1.color);
+        System.out.println(c1.fuelType);
+        System.out.println(c1.topSpeed);
+        System.out.println(c1.isRunning);
+    }
+}

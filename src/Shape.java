9017f9caa1affe2f369918d8b73cc7105955a5fe
Istiak_Ahamed <istiakop800@gmail.com>
2026-05-23 10:43:58 +0600

    Update Shape,Triangle,Test

---
+++ src/Shape.java
@@ -0,0 +1,8 @@
+public class Shape {
+    String color;
+
+    public void Display() {
+        System.out.println("Color: "+color);
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main (String[]args) {
+        Triangle t1 = new Triangle();
+        t1.color ="Blue";
+
+        t1.Display();
+
+    }
+}

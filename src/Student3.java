6a6d5b80c6445ad178714a677029db4c50cac474
Istiak_Ahamed <istiakop800@gmail.com>
2026-05-24 21:32:06 +0600

    Encapsulation

---
+++ src/Student3.java
@@ -0,0 +1,18 @@
+public class Student3 {
+        private String Name;
+        private int Age;
+
+        public Student3 (){}
+
+        public void Set(String Name,int Age) {
+            this.Name = Name;
+            this.Age = Age;
+        }
+        public String getName () {
+
+            return Name;
+        }
+        public int getAge () {
+            return Age;
+        }
+    }

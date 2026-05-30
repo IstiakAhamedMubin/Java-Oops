ab08b8ceeeb42d8ff6474b0505397b95df9b9950
Istiak_Ahamed <istiakop800@gmail.com>
2026-05-30 22:11:56 +0600

    ThrowKeyword uses

---
+++ src/ThrowKeyword.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ThrowKeyword {
+
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter Age: ");
+            int age = scanner.nextInt();
+
+            if (age < 18) {
+                throw new IllegalArgumentException("Access Denied");
+            }
+            System.out.println("Access Granted");
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+}
